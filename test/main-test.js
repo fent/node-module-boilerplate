f5bf0d9079d100279e41b61992334da15d507a5b
fent <933490+fent@users.noreply.github.com>
2018-09-22 19:09:35 -0400

    refactor: es6 arrow functions, `const`

--- test/main-test.js
+++ test/main-test.js
@@ -1,8 +1,8 @@
-var lib    = require('..');
-var assert = require('assert');
+const lib    = require('..');
+const assert = require('assert');
 
 
-describe('Test', function() {
-  it('Should do what I tell it to', function(done) {
+describe('Test', () => {
+  it('Should do what I tell it to', (done) => {
   });
 });
